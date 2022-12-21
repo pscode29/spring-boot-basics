74675fdf1baa1370990fac789a3a3257126a1f61
Pushpraj Singh <thepushpraj@live.com>
2022-12-21 19:33:30 +0000

    inversion of control

--- src/main/java/com/pscode/springbootbasics/SpringBootBasicsApplication.java
+++ src/main/java/com/pscode/springbootbasics/SpringBootBasicsApplication.java
@@ -1,13 +1,24 @@
 /**
- * Following Code explains how spring creates and manages beans on your behalf. Beans are nothing but objects
- * that are created and managed by spring for you. The container object that manages these other objects(beans) is
- * called as the Application Context.
+ * IoC: Inversion of Control
+ * Following code shows how you let spring manage your objects as a bean using @Component annotation. This
+ * way you have inverted the control of your object creation and management etc. to spring.
+ *
+ * Benefits:
+ * - Allows decoupling - Domain Driven Design stuff
+ * - DRY: You don't have to keep instantiating your object every time it is needed
+ * - Share a resource throughout the app and don't worry about lifecycle management of objects
+ * - Helps in testing by letting you mock objects easily, we will see that later.
+ *
+ * NOTE - Dependency Injection is a way of achieving IoC.
  */
 package com.pscode.springbootbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class SpringBootBasicsApplication {
@@ -24,5 +35,22 @@ public class SpringBootBasicsApplication {
 
 		}
 
+//		Clock clock = new Clock();
+//		LocalDateTime localDateTime = clock.getLocalDateTime();
+//		System.out.println("localDateTime = " + localDateTime);
+
+		Clock clock = applicationContext.getBean("clock", Clock.class);
+		LocalDateTime localDateTime = clock.getLocalDateTime();
+		System.out.println("localDateTime = " + localDateTime);
+
+	}
+}
+
+@Component
+class Clock {
+	private LocalDateTime localDateTime = LocalDateTime.now();
+
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
 	}
 }
